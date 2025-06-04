5da8e18bf300697777e08789534613e3053655ad
iman cheema <cheema.iman2@gmail.com>
2025-06-04 13:12:45 -0400

    Adds navbar component + styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
+import logoImg from "../assets/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <span className="phi">φ</span>
-        <span className="brand-name">fAI</span>
+        <img src={logoImg} alt="fAI logo" className="logo-image" />
       </div>
-      <div className="nav-links">
+      {/* <div className="nav-links">
         <Link to="/">1. Upload</Link>
         <Link to="/evaluate">2. Evaluate</Link>
         <Link to="/make-fair">3. Make Fair</Link>
-      </div>
+      </div> */}
     </nav>
   );
 }
