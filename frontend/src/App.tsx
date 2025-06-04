a299a85a91f2a8160cce259b413c7073b3f4a2f9
iman cheema <cheema.iman2@gmail.com>
2025-06-04 13:14:16 -0400

    Routing for upload + evaluate screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import UploadModel from "./screens/UploadModel";
+import Evaluate from "./screens/Evaluate";
+import LandingPage from "./screens/LandingPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<UploadModel />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/upload" element={<UploadModel />} />
+        <Route path="/evaluate" element={<Evaluate />} />
       </Routes>
     </Router>
   );
