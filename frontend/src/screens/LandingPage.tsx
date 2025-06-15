2deb335f08725226518af29cbc9cbc2bcf752e3c
iman cheema <cheema.iman2@gmail.com>
2025-06-15 00:07:57 -0400

    Fixes landing page colour scheme

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import heroImg from "../assets/hero.png";
 import "./LandingPage.css";
+import "../App.css";
+
 import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
@@ -13,18 +15,15 @@ const LandingPage = () => {
       <header className="landing-header">
         <div className="landing-content">
           <div className="text-section">
-            <h1>
-              Does your AI <span className="highlight">judge a book</span> by
-              its cover?
-            </h1>
+            <h1>Your Model Makes Decisions. But Are They Fair?</h1>
             <h2 className="subtitle">
-              Because great resumes don't all look the same.
+              Because fair decisions start with fair algorithms.
             </h2>
-            <p>
+            {/* <p>
               Upload your resume screening model, and we’ll help you uncover how
               fair they truly are. No fluff, just quick fairness metrics and
               clear results so you can see if your AI is playing fair.
-            </p>
+            </p> */}
             <button className="get-started-btn" onClick={handleGetStarted}>
               Get Started
             </button>
